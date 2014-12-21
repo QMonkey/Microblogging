9a4ad24a2e43c473758ed018984ba3c4454bfa00
QYoCiMonkey <qyocimonkey@linux-uef9.site>
2014-12-21 22:40:17 +0800

    Make project runnable.

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var path = require("path");
+var router = express.Router();
+
+router.get("/", function(request, response) {
+	response.sendFile(path.join(path.dirname(module.parent.filename), "/views/index.html"));
+});
+
+module.exports = router;
